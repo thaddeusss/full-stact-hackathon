56eb3c4458d7b87fb1ac3b9625bd0bf47ab4d9c8
sharshenalievbarni@gmail.com <sharshenalievbarni@gmail.com>
2022-08-25 11:05:15 +0600

    ne do delonnoe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import ProductContextProvider from "./contexts/ProductContextPovider";
+import MainRoutes from "./MainRoutes";
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <ProductContextProvider>
+
+      <MainRoutes />
+    </ProductContextProvider>
+
+  </div>;
 }
 
 export default App;
--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutUsPage from "./pages/AboutUsPage";
+import  AdminPage from "./pages/AdminPage";
+import ProductCardPage from "./pages/ProductCardPage";
+
+
+
+
 
 const MainRoutes = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      
+      
+      <Route path="/add" element={<AdminPage />} />
+      <Route path="/Products" element={<ProductCardPage />} />
+      <Route path="/About" element={<AboutUsPage />} />
+
+
+    
+    </Routes>
+  );
 };
 
 export default MainRoutes;
